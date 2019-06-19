ee3cc28d2804e806aced76a9e28e8a951b90d0d3
noahxyao <xiufuyao@gmail.com>
2019-06-19 16:02:23 +0200

    Added Crouching, Non-sticky character and coin

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     void Start()
     {
@@ -20,11 +22,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
     }
 
     void FixedUpdate ()
     {
         // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
     }
 }
