2debc716d6c65e374cc4fba92534d7b3964f1c53
Helihub <haminh.thieu@gmail.com>
2019-06-20 17:37:08 +0200

    Add BoxControl, Boxes which appear if keys B-O-X were typed

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -7,7 +7,9 @@ public class Respawn : MonoBehaviour
     public float threshold;
  
      void FixedUpdate () {
-         if (transform.position.y < threshold)
+         if (transform.position.y < threshold) {
              transform.position = new Vector3(-5.5f, 2, -2);
+	     BoxBehavior.enable = false;
+	 }
      }
 }
